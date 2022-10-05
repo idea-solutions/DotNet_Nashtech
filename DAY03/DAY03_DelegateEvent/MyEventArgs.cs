bb1281c1e1e7c20b8f7893714f2746b88671a37b
nvhoan <nvhoan911@gmail.com>
2022-10-05 15:47:34 +0700

    Updating app clock

---
+++ DAY03/DAY03_DelegateEvent/MyEventArgs.cs
@@ -0,0 +1,29 @@
+namespace DelegateEvent
+{
+    public class MyEventArgs : EventArgs
+    {
+        public MyEventArgs(int hour, int minute, int second)
+        {
+            this.hour = hour;
+            this.minute = minute;
+            this.second = second;
+        }
+
+        private int hour;
+        private int minute;
+        private int second;
+        public int Hour
+        {
+            get { return hour; }
+        }
+        public int Minute
+        {
+            get { return minute; }
+        }
+        public int Second
+        {
+            get { return second; }
+        }
+
+    }
+}
--- DAY03/DAY03_DelegateEvent/Program.cs
+++ DAY03/DAY03_DelegateEvent/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace DelegateEvent
+{
+    public class Program
+    {
+        // TODO: Chưa hoàn thiện, đang chỉnh sửa
+        static void Main(string[] args)
+        {
+            Clock p = new Clock();
+            Display sa = new Display();
+
+            sa.Sub(p); // sa đăng ký nhận sự kiện từ p
+            while (!Console.KeyAvailable)
+            {
+                Thread.Sleep(1000);
+                p.Send();
+            }
+        }
+    }
+
+
+    // Xây dựng lớp, phát đi sự kiện (data)
+    public class Clock
+    {
+        // Tạo delegate
+        public delegate void clockTick(object clock, MyEventArgs myEventArgs);
+        // Tạo Event với EventHandler
+        public event clockTick clockTickEvent;
+        public void Send()
+        {
+            DateTime currentTime = DateTime.Now;
+            clockTickEvent?.Invoke(this, new MyEventArgs(currentTime.Hour, currentTime.Minute, currentTime.Second));
+        }
+    }
+
+    public class Display
+    {
+        public static void ShowClock(object clock, MyEventArgs myEventArgs)
+        {
+            DateTime currentTime = DateTime.Now;
+            Console.WriteLine($"{myEventArgs.Hour}:{myEventArgs.Minute}:{myEventArgs.Second}");
+        }
+        public void Sub(Clock p)
+        {
+            p.clockTickEvent += new Clock.clockTick(ShowClock);
+        }
+    }
+
+}
