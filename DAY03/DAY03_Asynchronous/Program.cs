45113c51fe207d34ca5c40142fe44230672c8574
nvhoan <nvhoan911@gmail.com>
2022-10-05 14:23:39 +0700

    Add Day03 Asynchronous

---
+++ DAY03/DAY03_Asynchronous/Program.cs
@@ -0,0 +1,41 @@
+﻿
+namespace Asynchronous
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            PrimeNumber(0, 50);
+            PrimeNumber(51, 100);
+            Console.ReadKey();
+        }
+
+        public static async void PrimeNumber(int firstNumber, int lastNumber)
+        {
+            await Task.Run(async () =>
+            {
+                int count = 0;
+                for (int i = firstNumber; i < lastNumber; i++)
+                {
+                    count = 0;
+                    if (i > 1)
+                    {
+                        for (int j = 2; j < i; j++)
+                        {
+                            if (i % j == 0)
+                            {
+                                count = 1;
+                                break;
+                            }
+                        }
+                        if (count == 0)
+                        {
+                            Console.Write(i + " ");
+                            await Task.Delay(200);
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
