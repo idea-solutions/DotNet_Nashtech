e12add7292c522bfb30a04eff6e240545f15a85d
nvhoan <nvhoan911@gmail.com>
2022-11-05 17:10:37 +0700

    Update Folder Mid Assignment

---
+++ Mid_Assignment/server/Data/Migrations/20221105100134_FirstMigration.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    public partial class FirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "People",
+                columns: table => new
+                {
+                    PersonId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: true),
+                    Dob = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Gender = table.Column<int>(type: "int", nullable: true),
+                    EmailAddress = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LicenseId = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_People", x => x.PersonId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "People");
+        }
+    }
+}
---
+++ Mid_Assignment/server/WebAPI/Services/ITestService.cs
@@ -0,0 +1,11 @@
+﻿using Data.Entities;
+
+namespace WebAPI.Services
+{
+    public interface ITestService
+    {
+        Task<IEnumerable<Person>> Get();
+
+        Task Add();
+    }
+}
