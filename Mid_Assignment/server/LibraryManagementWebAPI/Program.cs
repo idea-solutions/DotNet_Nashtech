9440b1c3174e4feb83d648ff09d2efa633886d65
nvhoan <nvhoan911@gmail.com>
2022-11-09 16:02:12 +0700

    AddCors config

--- Mid_Assignment/server/LibraryManagementWebAPI/Program.cs
+++ Mid_Assignment/server/LibraryManagementWebAPI/Program.cs
@@ -35,6 +35,17 @@ builder.Services.AddTransient<IUserService, UserService>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+        {
+            builder
+                .AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -47,6 +58,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors();
 
 app.MapControllers();
 
